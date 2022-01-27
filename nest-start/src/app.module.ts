e65ba20936281f5c5c49c9ad76984d104bdd93c2
wuyaxiong <wuyaxiong@fcbox.com>
2022-01-27 17:57:23 +0800

    feature: 控制器-路由参数

--- nest-start/src/app.module.ts
+++ nest-start/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats.module';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [CatsModule],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
